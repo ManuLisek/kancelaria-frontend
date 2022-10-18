e178aafb61bc28a3b2cb561e5cadbcaeb2a0b529
Michał Lisowiec <miclisowiec@gmail.com>
2022-10-18 22:00:08 +0200

    KAN-6-Dividers: Add container and media queries

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -1,18 +1,32 @@
+import Container from '@mui/material/Container';
 import styled from 'styled-components';
 
 const DividerContainer = styled.div<DividerContainerProps>`
   display: flex;
   align-items: center;
   justify-content: center;
-  text-align: center;
-  padding: 80px 60px;
+  margin-bottom: 60px;
+  padding: 40px 0px;
   width: 100%;
   border-bottom-right-radius: 70px;
+  background: url(${(props) => props.image}) no-repeat fixed;
+  background-size: cover;
+
+  @media (max-width: 599px) {
+    margin-bottom: 30px;
+    padding: 20px 0px;
+  }
+`;
+
+const Title = styled.h2`
+  text-align: center;
   font-size: 48px;
   font-weight: bold;
   color: white;
-  background: url(${(props)=> props.image}) no-repeat fixed;
-  background-size: cover;
+
+  @media (max-width: 599px) {
+    font-size: 28px;
+  }
 `;
 
 interface DividerContainerProps {
@@ -21,13 +35,14 @@ interface DividerContainerProps {
 
 interface DividerProps extends DividerContainerProps {
   title: string;
-};
+}
 
-const Divider = ({title, image}: DividerProps): JSX.Element => {
-
-  return (
-    <DividerContainer image={image}>{title}</DividerContainer>
-  )
-};
+const Divider = ({ title, image }: DividerProps): JSX.Element => (
+  <DividerContainer image={image}>
+    <Container>
+      <Title>{title}</Title>
+    </Container>
+  </DividerContainer>
+);
 
 export default Divider;
