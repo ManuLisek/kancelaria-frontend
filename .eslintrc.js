baa02fc6cf9606a7504521f3f0134529d11a26b5
Michał Lisowiec <miclisowiec@gmail.com>
2022-10-19 22:37:01 +0200

    Fix eslint, create folder for data

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,20 +14,12 @@ module.exports = {
     node: true,
   },
   rules: {
-<<<<<<< HEAD
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'linebreak-style': 'off',
-    'import/prefer-default-export': 'off',
-    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
-=======
     "react/jsx-props-no-spreading": "off",
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
     "linebreak-style": "off",
     "import/prefer-default-export": "off",
     "react/function-component-definition": [2, { "namedComponents": "arrow-function" }],
->>>>>>> 174059cd241288be7687abbbe76f8b53a3ce9a08
   },
   settings: {
     react: {
---
+++ data/specsData.ts
@@ -0,0 +1,71 @@
+import GavelIcon from '@mui/icons-material/Gavel';
+import PetsIcon from '@mui/icons-material/Pets';
+import HomeIcon from '@mui/icons-material/Home';
+import FamilyRestroomIcon from '@mui/icons-material/FamilyRestroom';
+import WorkIcon from '@mui/icons-material/Work';
+import ImportContactsIcon from '@mui/icons-material/ImportContacts';
+import BusinessIcon from '@mui/icons-material/Business';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import PersonIcon from '@mui/icons-material/Person';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+
+const specsData = [
+  {
+    id: 1,
+    title: 'Prawo karne',
+    icon: GavelIcon,
+  },
+  {
+    id: 2,
+    title: 'Prawo cywilne',
+    icon: PersonIcon,
+  },
+  {
+    id: 3,
+    title: 'Nieruchomości',
+    icon: HomeIcon,
+  },
+  {
+    id: 4,
+    title: 'Prawo rodzinne',
+    icon: FamilyRestroomIcon,
+  },
+  {
+    id: 5,
+    title: 'Prawo spadkowe',
+    icon: ImportContactsIcon,
+  },
+  {
+    id: 6,
+    title: 'Prawo administracyjne',
+    icon: ManageAccountsIcon,
+  },
+  {
+    id: 7,
+    title: 'Prawo pracy',
+    icon: WorkIcon,
+  },
+  {
+    id: 8,
+    title: 'Prawa spółek',
+    icon: BusinessIcon,
+  },
+  {
+    id: 9,
+    title: 'Ochrona praw zwierząt',
+    icon: PetsIcon,
+  },
+  {
+    id: 10,
+    title: 'Ubezpieczenia',
+    icon: HealthAndSafetyIcon,
+  },
+  {
+    id: 11,
+    title: 'Dochodzenie odszkodowań',
+    icon: MonetizationOnIcon,
+  },
+];
+
+export default specsData;
